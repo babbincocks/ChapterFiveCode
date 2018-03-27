461463c42e6b438077d07df12d98173738e12dc9
babbincocks <zabbybab@gmail.com>
2018-03-27 18:23:33 -0400

    Finished the second tutorial.

---
+++ ChapterFiveCode/Ending Balance Modified/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Ending_Balance_Modified
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.endingBalanceLabel = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.monthsTextBox = new System.Windows.Forms.TextBox();
+            this.startingBalTextBox = new System.Windows.Forms.TextBox();
+            this.monthsPromptLabel = new System.Windows.Forms.Label();
+            this.startingBalancePromptLabel = new System.Windows.Forms.Label();
+            this.detailListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(205, 234);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 28;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(109, 234);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 27;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click_1);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(14, 234);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 26;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // endingBalanceLabel
+            // 
+            this.endingBalanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.endingBalanceLabel.Location = new System.Drawing.Point(135, 192);
+            this.endingBalanceLabel.Name = "endingBalanceLabel";
+            this.endingBalanceLabel.Size = new System.Drawing.Size(100, 23);
+            this.endingBalanceLabel.TabIndex = 25;
+            this.endingBalanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(44, 197);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(85, 13);
+            this.outputDescriptionLabel.TabIndex = 24;
+            this.outputDescriptionLabel.Text = "Ending Balance:";
+            // 
+            // monthsTextBox
+            // 
+            this.monthsTextBox.Location = new System.Drawing.Point(135, 40);
+            this.monthsTextBox.Name = "monthsTextBox";
+            this.monthsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.monthsTextBox.TabIndex = 23;
+            // 
+            // startingBalTextBox
+            // 
+            this.startingBalTextBox.Location = new System.Drawing.Point(135, 13);
+            this.startingBalTextBox.Name = "startingBalTextBox";
+            this.startingBalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.startingBalTextBox.TabIndex = 22;
+            // 
+            // monthsPromptLabel
+            // 
+            this.monthsPromptLabel.AutoSize = true;
+            this.monthsPromptLabel.Location = new System.Drawing.Point(32, 43);
+            this.monthsPromptLabel.Name = "monthsPromptLabel";
+            this.monthsPromptLabel.Size = new System.Drawing.Size(97, 13);
+            this.monthsPromptLabel.TabIndex = 21;
+            this.monthsPromptLabel.Text = "Number of Months:";
+            // 
+            // startingBalancePromptLabel
+            // 
+            this.startingBalancePromptLabel.AutoSize = true;
+            this.startingBalancePromptLabel.Location = new System.Drawing.Point(41, 17);
+            this.startingBalancePromptLabel.Name = "startingBalancePromptLabel";
+            this.startingBalancePromptLabel.Size = new System.Drawing.Size(88, 13);
+            this.startingBalancePromptLabel.TabIndex = 20;
+            this.startingBalancePromptLabel.Text = "Starting Balance:";
+            // 
+            // detailListBox
+            // 
+            this.detailListBox.FormattingEnabled = true;
+            this.detailListBox.Location = new System.Drawing.Point(12, 78);
+            this.detailListBox.Name = "detailListBox";
+            this.detailListBox.Size = new System.Drawing.Size(268, 95);
+            this.detailListBox.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 288);
+            this.Controls.Add(this.detailListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.endingBalanceLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.monthsTextBox);
+            this.Controls.Add(this.startingBalTextBox);
+            this.Controls.Add(this.monthsPromptLabel);
+            this.Controls.Add(this.startingBalancePromptLabel);
+            this.Name = "Form1";
+            this.Text = "Ending Balance (Enhanced)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label endingBalanceLabel;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.TextBox monthsTextBox;
+        private System.Windows.Forms.TextBox startingBalTextBox;
+        private System.Windows.Forms.Label monthsPromptLabel;
+        private System.Windows.Forms.Label startingBalancePromptLabel;
+        private System.Windows.Forms.ListBox detailListBox;
+    }
+}
+
---
+++ ChapterFiveCode/Ending Balance Modified/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ending_Balance_Modified
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+
+            //Constant for the monthly interest rate.
+            const decimal INTEREST_RATE = 0.005m;
+
+            //Local variables.
+            decimal balance;
+            int months;
+            int count = 1;
+
+            //Get the starting balance.
+            if (decimal.TryParse(startingBalTextBox.Text, out balance))
+            {
+                //Get the number of months.
+                if (int.TryParse(monthsTextBox.Text, out months))
+                {
+                    //The following loop calculates the end balance.
+                    while (count <= months)
+                    {
+                        //This adds this month's interest to the balance.
+                        balance = balance + (INTEREST_RATE * balance);
+
+                        //This displays this month's ending balance.
+                        detailListBox.Items.Add("The ending balance for month " + count + " is "
+                            + balance.ToString("c"));
+
+                        //Increment the counter.
+                        count++;
+                    }
+
+                    //Display the ending balance.
+                    endingBalanceLabel.Text = balance.ToString("c");
+
+                }
+                else
+                {
+                    //Invalid number of months was entered.
+                    MessageBox.Show("The entered value for the number of months was invalid.");
+                }
+            }
+            else
+            {
+                //Invalid starting balance was entered.
+                MessageBox.Show("The entered value for your balance was invalid.");
+
+            }
+        }
+
+        private void clearButton_Click_1(object sender, EventArgs e)
+        {
+            //Resets the field values.
+            startingBalTextBox.Text = "";
+            monthsTextBox.Text = "";
+            endingBalanceLabel.Text = "";
+            detailListBox.Items.Clear();
+
+            //Resets the focus.
+            startingBalTextBox.Focus();
+        }
+
+        private void exitButton_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
